472e8ed212d2cce797b861aae982e3d0f945b6f1
Howaboutthis <petrenkoivan96@gmail.com>
2017-12-27 16:47:00 +0300

    coroutines

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,53 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
 
-void Engine::Restore(context &ctx) {}
+void Engine::Store(context &ctx) {
+    char test;
+    char* where=&test;
+    uint32_t len=StackBottom-where;
+    if (std::get<0>(ctx.Stack)!=nullptr)
+	 delete [](std::get<0>(ctx.Stack));
+    std::get<0>(ctx.Stack)=new char[len];
+    std::get<1>(ctx.Stack)=len;
+    memcpy(std::get<0>(ctx.Stack),where,len);
+}
 
-void Engine::yield() {}
 
-void Engine::sched(void *routine_) {}
+void Engine::Restore(context &ctx) {
+    char test;
+    char* where=&test;
+    if (where+std::get<1>(ctx.Stack)>StackBottom){
+        Restore(ctx);
+    }
+    memcpy(StackBottom - std::get<1>(ctx.Stack),std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
+    longjmp(ctx.Environment,1);
+}
+
+void Engine::yield() {
+    context* routine=alive;
+    if (routine!=nullptr){
+        this->alive=routine->next;
+        sched(routine);
+  } 
+}
+
+void Engine::sched(void *routine_) {
+    if (routine_==nullptr || routine_==cur_routine)
+	return;
+    if (cur_routine==nullptr){
+        cur_routine=static_cast<context *>(routine_);
+        Restore(*cur_routine);
+    }
+    if (setjmp(cur_routine->Environment)>0){
+        return;
+    }
+    else{
+       Store(*cur_routine);
+       cur_routine=static_cast<context *>(routine_);
+       Restore(*cur_routine);
+    }
+}
 
 } // namespace Coroutine
 } // namespace Afina
